10a4a216f782c6fef10c70fc3c52f33665372866
Caiqe <cttcaiquegomes@gmail.com>
2025-02-13 16:22:03 -0300

    Alterações NavBar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,69 @@
+import { Basket, List, MagnifyingGlass } from "@phosphor-icons/react";
 import React from "react";
 import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
 
 function Navbar() {
   return (
     <>
-      <div className="w-full flex flex-row justify-around bg-red-700 py-1">
-        <div>
+      <div className="fixed mt-0 w-full grid grid-cols-12 bg-gray-800 py-4">
+        <div className="col-span-2 col-start-1">
           <Link
             to="/home"
-            className="hover:underline text-white font-bold text-6xl"
+            className="hover:underline  text-white font-bold text-4xl"
           >
-            Farmácia
+            <h2>Farmárcia</h2>
           </Link>
         </div>
-        <div className="flex flex-row gap-4 font-bold text-white text-3xl">
-          <p>Produtos</p>
-          <Link to="/categorias" className="hover:underline">
-            Categorias
-          </Link>
-          <Link to="/cadastrarcategoria" className="hover:underline">
-            Cadastrar Categoria
-          </Link>
-          <p>usuário</p>
+        <div className="flex flex-row col-start-5 col-span-4 bg-white rounded-4xl justify-between items-center px-6">
+          <form action="" className=" p-3  text-black">
+            <input
+              type="text"
+              placeholder="O que você precisa?"
+              className="border-0"
+            />
+          </form>
+
+          <MagnifyingGlass
+            size={30}
+            color="#121212"
+            weight="bold"
+            className=" border-l-2 pl-2 hover:cursor-pointer"
+          />
         </div>
+        <div className="flex flex-row gap-8 col-start-12 col-span-2">
+          <Popup
+            trigger={
+              <div className="">
+                {" "}
+                <List
+                  size={32}
+                  color="#fffafa"
+                  weight="bold"
+                  className="hover:cursor-pointer hover:scale-110 rounded-full  "
+                />
+              </div>
+            }
+            on="hover"
+            closeOnDocumentClick
+            mouseLeaveDelay={100}
+            mouseEnterDelay={0}
+            contentStyle={{ padding: "0px", border: "none" }}
+            arrow={false}
+          >
+            <div className="bg-black text-white hover:cursor-pointer p-6 rounded-3xl">
+              <div className="hover:underline">
+                {" "}
+                <Link to="/categorias">Categorias</Link>
+              </div>
+              <div className="hover:underline"> item 2</div>
+              <div className="hover:underline"> item 3</div>
+            </div>
+          </Popup>
+          <Basket size={32} color="#fffafa" weight="bold" />
+        </div>
+
+        <div className="flex flex-row gap-4 font-bold text-white text-3xl"></div>
       </div>
     </>
   );
